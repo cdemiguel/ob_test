cf5b43dc5c2c85f61c039987e23e39afa13f7ac6
cdemiguel <demiguelferrerocarlos@gmail.com>
2018-04-03 12:16:32 +0200

    error handling on routes

--- server-api/src/routes/index.js
+++ server-api/src/routes/index.js
@@ -1,13 +1,23 @@
-const logic = require("../logic")
 const express = require("express")
 const eventRoute = express.Router()
 
-const events= require('../data/events.json')
-const eventInfo184 = require('../data/event-info-184.json')
-const eventInfo68 = require('../data/event-info-68.json')
+const events = require("../data/events.json")
+const eventInfo184 = require("../data/event-info-184.json")
+const eventInfo68 = require("../data/event-info-68.json")
 
-eventRoute.get('/', (req, res) => {
-    res.json(events)
-});
+eventRoute.get("/", (req, res) => {
+  if (events.length) {
+    res.json({
+      status: "OK",
+      message: "events recieved OK!",
+      data: events
+    })
+  } else {
+    res.json({
+      status: "KO",
+      message: err.message
+    })
+  }
+})
 
 module.exports = eventRoute
